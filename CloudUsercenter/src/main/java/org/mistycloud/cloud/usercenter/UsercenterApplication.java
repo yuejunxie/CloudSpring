a3eb814ba948650506ad960ef54275ef18a9c203
xieyuejun <xieyuejun@hikvision.com.cn>
2018-12-17 18:46:23 +0800

    配置修改

--- CloudUsercenter/src/main/java/org/mistycloud/cloud/usercenter/UsercenterApplication.java
+++ CloudUsercenter/src/main/java/org/mistycloud/cloud/usercenter/UsercenterApplication.java
@@ -2,10 +2,8 @@ package org.mistycloud.cloud.usercenter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
@@ -14,9 +12,9 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  */
 @EnableScheduling
 @EnableFeignClients
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @EnableEurekaClient
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication
 public class UsercenterApplication {
 
     public static void main(String[] args) {
