54d8e6a0f99141951ff28e99fa070ecf36719eb6
xieyuejun <xieyuejun@hikvision.com.cn>
2018-12-20 19:30:41 +0800

    提交修改

--- CloudConsul/src/main/java/com/mistycloud/consul/ConsulApplication.java
+++ CloudConsul/src/main/java/com/mistycloud/consul/ConsulApplication.java
@@ -1,24 +1,8 @@
 package com.mistycloud.consul;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
-import org.springframework.cloud.client.serviceregistry.Registration;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.List;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author xieyuejun
@@ -26,6 +10,7 @@ import java.util.List;
  */
 @SpringBootApplication
 //@EnableFeignClients
+@EnableDiscoveryClient
 public class ConsulApplication {
 
     public static void main(String[] args) {
--- CloudResource/src/main/java/org/mistycloud/cloud/resource/ResourceApplication.java
+++ CloudResource/src/main/java/org/mistycloud/cloud/resource/ResourceApplication.java
@@ -3,16 +3,13 @@ package org.mistycloud.cloud.resource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 /**
  * @author xieyuejun
  * @created 2018/4/12 17:07
  */
-@EnableEurekaClient
 @EnableDiscoveryClient
-@EnableFeignClients
 @SpringBootApplication
 public class ResourceApplication {
 
