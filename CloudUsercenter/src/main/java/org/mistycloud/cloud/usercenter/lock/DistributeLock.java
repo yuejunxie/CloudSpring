8f29f7977f89d5704fcc755378a4cc5fae381673
xieyuejun <xieyuejun@hikvision.com.cn>
2019-01-21 11:57:49 +0800

    锁

---
+++ CloudUsercenter/src/main/java/org/mistycloud/cloud/usercenter/lock/DistributeLock.java
@@ -0,0 +1,9 @@
+package org.mistycloud.cloud.usercenter.lock;
+
+/**
+ * @author xieyuejun
+ * @created 2019/1/21 11:56
+ */
+public class DistributeLock {
+//    https://github.com/yangliu0/DistributedLock/blob/master/src/main/java/zookeeper/DistributedLock.java
+}
---
+++ CloudUsercenter/src/main/java/org/mistycloud/cloud/usercenter/lock/zookeeper/ZookeeperLock.java
@@ -0,0 +1,8 @@
+package org.mistycloud.cloud.usercenter.lock.zookeeper;
+
+/**
+ * @author xieyuejun
+ * @created 2019/1/21 11:56
+ */
+public class ZookeeperLock {
+}
